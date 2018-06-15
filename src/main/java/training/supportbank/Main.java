9b715bc929575f46df9c1d77399c119ae46b66ad
Julian Ng <julian.ng@softwire.com>
2018-06-15 13:50:42 +0100

    FIRST: created simple file reader outputting file contents

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,22 @@
 package training.supportbank;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String args[]) {
-        // Your code here!
-        System.out.println("Test!");
+    public static void main(String args[]) throws IOException {
+        String csvFile = "Transactions2014.csv";
+        BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+
+        Scanner scanner = new Scanner(reader);
+        scanner.useDelimiter(",");
+
+        while (scanner.hasNext()) {
+            System.out.print(scanner.next() + " ¦ ");
+        }
+
+        scanner.close();
     }
 }
---
+++ src/main/java/training/supportbank/Printer.java
@@ -0,0 +1,7 @@
+package training.supportbank;
+
+public class Printer {
+    public static String listAll() {
+        return null;
+    }
+}
