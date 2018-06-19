77d3679b727d0ebcde82a4c2c25cf13d2174f1da
Julian Ng <julian.ng@softwire.com>
2018-06-19 14:33:31 +0100

    Created console interaction to select operation

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -6,9 +6,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) throws IOException {
         Printer printer = new Printer();
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("This programme allows you to view processed data for the Transaction2014.csv file.\n" +
+                "ListAll - outputs each persons name and the total they owe or are owed\n" +
+                "ListAccount - outputs every transaction associated with the given name\n" +
+                "Please enter your desired operation: ");
+        String operation = scanner.next();
+        if (operation.equals("ListAll")) {
+            printer.listAll("Transactions2014.csv");
+        } else if (operation.equals("ListAccount")) {
+            System.out.print("Enter the full name of the person whose transactions you wish to view: ");
+            String accountName = scanner.next();
+            accountName += scanner.nextLine();
+            printer.listAccount("Transactions2014.csv", accountName);
+        } else {
+            System.out.print("Invalid input");
+        }
 
-//        printer.listAll("Transactions2014.csv");
-        printer.listAccount("Transactions2014.csv", "Jon A");
     }
 }
