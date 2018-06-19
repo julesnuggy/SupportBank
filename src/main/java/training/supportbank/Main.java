891ed2973715db026ea74b47632d936057a6a237
Julian Ng <julian.ng@softwire.com>
2018-06-19 13:29:57 +0100

    Started setting up code for List Account functionality

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,10 +1,13 @@
 package training.supportbank;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         Printer printer = new Printer();
+//        Scanner scanner = new Scanner(System.in);
+
         printer.listAll("Transactions2014.csv");
     }
 }
--- src/main/java/training/supportbank/Printer.java
+++ src/main/java/training/supportbank/Printer.java
@@ -9,11 +9,12 @@ import java.util.Map;
 import java.util.Set;
 
 public class Printer {
+    private static DataHandler dataHandler;
+    private static BufferedReader reader;
+    private static PrinterFormatter formatter;
+
     public static void listAll(String csvFile) throws IOException {
-        DataHandler dataHandler = new DataHandler();
-        BufferedReader reader = new BufferedReader(new FileReader(csvFile));
-        PrinterFormatter formatter = new PrinterFormatter();
-        reader.readLine();
+        prepareFile(csvFile);
         List<Map<String, String>> transactionsToPrint = dataHandler.dataConverter(reader);
         Set<String> namesToUse = dataHandler.extractNames(transactionsToPrint);
         Map<String, BigDecimal> calculatedMap = dataHandler.calculateBalance(transactionsToPrint, namesToUse);
@@ -22,4 +23,18 @@ public class Printer {
             System.out.println(element);
         }
     }
+
+    public static void listAccount(String csvFile, String accountName) throws IOException {
+        prepareFile(csvFile);
+
+    }
+
+    private static void prepareFile(String csvFile) throws IOException {
+        dataHandler = new DataHandler();
+        reader = new BufferedReader(new FileReader(csvFile));
+        formatter = new PrinterFormatter();
+        reader.readLine();
+    }
+
+
 }
